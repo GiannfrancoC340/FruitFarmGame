496ccaa7cee19560571a8da720102ce82d58c97b
Giannfranco Crovetto <gcrovetto2021@fau.edu>
2025-02-07 21:52:45 -0500

    Added an apple as the first fruit.

--- FruitFarm/src/App.jsx
+++ FruitFarm/src/App.jsx
@@ -4,6 +4,10 @@ const App = () => {
 
   return (
     <div className="App">
+      <h6>Built by: Giannfranco Crovetto, Z23622354</h6>
+      <h1>Fruit Farm</h1>
+      <h2>Count: </h2>
+      <img className="apple" src="https://e1.pngegg.com/pngimages/14/397/png-clipart-fruit-red-apple-fruit-thumbnail.png" alt="apple"/>
 
     </div>
   )
