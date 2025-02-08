e599f783b422ae3e5a159661050c95f174753a36
Giannfranco Crovetto <gcrovetto2021@fau.edu>
2025-02-07 21:59:13 -0500

    Added 4 more fruits

--- FruitFarm/src/App.jsx
+++ FruitFarm/src/App.jsx
@@ -7,7 +7,11 @@ const App = () => {
       <h6>Built by: Giannfranco Crovetto, Z23622354</h6>
       <h1>Fruit Farm</h1>
       <h2>Count: </h2>
-      <img className="apple" src="https://e1.pngegg.com/pngimages/14/397/png-clipart-fruit-red-apple-fruit-thumbnail.png" alt="apple"/>
+      <img className="apple" src="https://pngimg.com/d/apple_PNG12439.png" alt="apple"/>
+      <img className="orange" src="https://png.pngtree.com/element_our/png/20180903/orange-png-png_75700.jpg" alt="orange"/>
+      <img className="banana" src="https://png.pngtree.com/png-clipart/20220716/ourmid/pngtree-banana-yellow-fruit-banana-skewers-png-image_5944324.png" alt="banana"/>
+      <img className="raspberry" src="https://static.vecteezy.com/system/resources/previews/022/825/589/non_2x/raspberry-fruit-raspberries-on-transparent-background-png.png" alt="raspberry"/>
+      <img className="watermelon" src="https://static.vecteezy.com/system/resources/previews/034/333/584/non_2x/set-fresh-watermelon-fruit-and-sliced-on-isolated-transparent-background-free-png.png" alt="watermelon"/>
 
     </div>
   )
