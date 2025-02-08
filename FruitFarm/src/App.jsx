ff731f151315bb8403d58b2894a15db6b4ae10b7
Giannfranco Crovetto <gcrovetto2021@fau.edu>
2025-02-07 22:08:20 -0500

    Created new functionality to ensure clicking and state tracking is good

--- FruitFarm/src/App.jsx
+++ FruitFarm/src/App.jsx
@@ -1,18 +1,65 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  
+  const [count, setCount] = useState(0);
+
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
+
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+      setCount(count - 10)
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier( multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
 
   return (
     <div className="App">
+      <div className="header">
       <h6>Built by: Giannfranco Crovetto, Z23622354</h6>
       <h1>Fruit Farm</h1>
-      <h2>Count: </h2>
-      <img className="apple" src="https://pngimg.com/d/apple_PNG12439.png" alt="apple"/>
-      <img className="orange" src="https://png.pngtree.com/element_our/png/20180903/orange-png-png_75700.jpg" alt="orange"/>
+      <h2>Count: {count}</h2>
+      <img className="apple" src="https://pngimg.com/d/apple_PNG12439.png" alt="apple" onClick={updateCount} />
+      {/* <img className="orange" src="https://png.pngtree.com/element_our/png/20180903/orange-png-png_75700.jpg" alt="orange"/>
       <img className="banana" src="https://png.pngtree.com/png-clipart/20220716/ourmid/pngtree-banana-yellow-fruit-banana-skewers-png-image_5944324.png" alt="banana"/>
       <img className="raspberry" src="https://static.vecteezy.com/system/resources/previews/022/825/589/non_2x/raspberry-fruit-raspberries-on-transparent-background-png.png" alt="raspberry"/>
-      <img className="watermelon" src="https://static.vecteezy.com/system/resources/previews/034/333/584/non_2x/set-fresh-watermelon-fruit-and-sliced-on-isolated-transparent-background-free-png.png" alt="watermelon"/>
+      <img className="watermelon" src="https://static.vecteezy.com/system/resources/previews/034/333/584/non_2x/set-fresh-watermelon-fruit-and-sliced-on-isolated-transparent-background-free-png.png" alt="watermelon"/> */}
+      </div>
 
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 samosas</button>
+        </div>
+      </div>
     </div>
   )
 }
