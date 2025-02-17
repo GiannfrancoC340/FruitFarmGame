38de7a037b3ef5cef62d8b8cc2842e75cd388599
Giannfranco Crovetto <gcrovetto2021@fau.edu>
2025-02-16 22:04:21 -0500

    Changed the pic of banana

--- FruitFarm/src/App.jsx
+++ FruitFarm/src/App.jsx
@@ -87,7 +87,7 @@ const App = () => {
         <h2>Amount of $: {count}</h2>
         <img className="apple" src="https://pngimg.com/d/apple_PNG12439.png" alt="apple" onClick={updateCountApple} />
         <img className="orange" src="https://png.pngtree.com/element_our/png/20180903/orange-png-png_75700.jpg" alt="orange" onClick={updateCountOrange} />
-        <img className="banana" src="https://png.pngtree.com/png-clipart/20220716/ourmid/pngtree-banana-yellow-fruit-banana-skewers-png-image_5944324.png" alt="banana" onClick={updateCountBanana} />
+        <img className="banana" src="https://pngimg.com/d/banana_PNG842.png" alt="banana" onClick={updateCountBanana} />
         {/*<img className="raspberry" src="https://static.vecteezy.com/system/resources/previews/022/825/589/non_2x/raspberry-fruit-raspberries-on-transparent-background-png.png" alt="raspberry"/>
         <img className="watermelon" src="https://static.vecteezy.com/system/resources/previews/034/333/584/non_2x/set-fresh-watermelon-fruit-and-sliced-on-isolated-transparent-background-free-png.png" alt="watermelon"/>
        */}
